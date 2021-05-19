f701c1a3defdab2119b79f5152e1eb3860aa1eb9
gediminasjuska <g.juska98@gmail.com>
2021-05-19 19:15:39 +0300

    test

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,37 @@
-require('dotenv').config()
-console.log("labas")
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const router = require('./routes/routes')
+require("dotenv").config();
+console.log("viso gero");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const router = require("./routes/routes");
 
 const corsOptions = {
-  allowedHeaders: ['eventauth', 'Content-Type'],
-  exposedHeaders: ['eventauth']
-}
+  allowedHeaders: ["eventauth", "Content-Type"],
+  exposedHeaders: ["eventauth"],
+};
 
-mongoose.connect('mongodb://localhost/EventDB', {
+mongoose.connect("mongodb://localhost/EventDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
-})
+  useCreateIndex: true,
+});
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error'))
-db.once('open', () => console.log('Logged into database'))
-
-
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => console.log("Logged into database"));
 
 const app = express();
-app.use(cors(corsOptions))
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(cors(corsOptions));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use('/uploads', express.static('uploads'))
+app.use("/uploads", express.static("uploads"));
 
+app.use("/api/v1", router);
 
-app.use('/api/v1', router)
-
-
-app.listen(3000)
-
+app.listen(3000);
