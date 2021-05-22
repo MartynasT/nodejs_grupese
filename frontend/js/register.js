f3ee8211860ba6a9f43057e9d85500e6bf676ffc
gediminasjuska <g.juska98@gmail.com>
2021-05-23 00:37:22 +0300

    commit

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,69 +1,69 @@
-let url = 'http://localhost:3000/api/v1'
+let url = "http://localhost:3000/api/v1";
 
+document
+  .getElementById("user-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-document.getElementById('user-form').addEventListener('submit', async (event) => {
-  event.preventDefault()
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
 
-  let email = document.getElementById('email').value
-  let password = document.getElementById('password').value
+    if (!email || !password) return alert("Fill in form");
 
-  if (!email || !password) return alert('Fill in form')
+    let body = {
+      email,
+      password,
+    };
+    let response = await fetch(`${url}/user/signUp`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
 
-  let body = {
-    email,
-    password
-  }
-  let response = await fetch(`${url}/user/signUp`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(body)
-  })
-  
-  console.log(response)
-  if (response.status != 200) return alert('Something went wrong')
+    console.log(response);
+    if (response.status != 200) return alert("Something went wrong");
 
-  let data = await response.json()
+    let data = await response.json();
 
-  console.log(data)
+    console.log(data);
 
-  window.location.href = './login'
-
-})
+    window.location.href = "./login.html";
+  });
 
 //ORGANIZATOR ==================================
 
-document.getElementById('org-form').addEventListener('submit', async (event) => {
-    event.preventDefault()
-    let orgName = document.getElementById("org-name").value
-    let email = document.getElementById('email-org').value
-    let password = document.getElementById('password-org').value
-  
-    if (!email || !password || !orgName) return alert('Fill in form')
-  
+document
+  .getElementById("org-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    let orgName = document.getElementById("org-name").value;
+    let email = document.getElementById("email-org").value;
+    let password = document.getElementById("password-org").value;
+
+    if (!email || !password || !orgName) return alert("Fill in form");
+
     let body = {
       email,
       password,
       organization: orgName,
-      role: 'admin'
-    }
+      role: "admin",
+    };
     let response = await fetch(`${url}/user/signUp`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(body)
-    })
-    
-    console.log(response)
-    if (response.status != 200) return alert('Something went wrong')
-  
-    let data = await response.json()
-  
-    console.log(data)
-  
-    window.location.href = './login.html'
-  
-  })
-  
+      body: JSON.stringify(body),
+    });
+
+    console.log(response);
+    if (response.status != 200) return alert("Something went wrong");
+
+    let data = await response.json();
+
+    console.log(data);
+
+    window.location.href = "./login.html";
+  });
