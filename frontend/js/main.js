fd908b004a4b08d6f65ba7d1be5cc1fad9c93796
Laura Pilvelyte <laura.pilvelytee@gmail.com>
2021-05-25 20:08:20 +0300

    update

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -71,21 +71,30 @@ const setUpNavAdmin = async () => {
 
   }
   const setUpNav = async () => { 
-    const LoginBtn = document.createElement('button')
-    LoginBtn.innerText="Login"
-    LoginBtn.setAttribute("type", "button")
-    LoginBtn.setAttribute('onclick', 'location.href="./login.html"')
   
-    const RegisterBtn = document.createElement('button')
-    RegisterBtn.innerText="Register"
-    RegisterBtn.setAttribute("type", "button")
-    RegisterBtn.classList.add('dark-btn')
-    RegisterBtn.setAttribute('onclick', 'location.href="./register.html"')
-
-    const header = document.getElementById('header')
-  header.append(RegisterBtn)
-  header.append(LoginBtn)
- 
+    const HomeBtn = document.createElement('a')
+    HomeBtn.innerText="Home"
+    HomeBtn.href="./index.html"  
+  
+      const LoginBtn = document.createElement('button')
+      LoginBtn.innerText="Login"
+      LoginBtn.setAttribute("type", "button")
+      LoginBtn.setAttribute('onclick', 'location.href="./login.html"')
+    
+      const RegisterBtn = document.createElement('button')
+      RegisterBtn.innerText="Register"
+      RegisterBtn.setAttribute("type", "button")
+      RegisterBtn.classList.add('dark-btn')
+      RegisterBtn.setAttribute('onclick', 'location.href="./register.html"')
+  
+  
+  
+      const header = document.getElementById('header')
+         
+    header.append(HomeBtn)
+    header.append(RegisterBtn)
+    header.append(LoginBtn)
+   
 
   }
 
